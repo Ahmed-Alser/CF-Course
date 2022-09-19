448cdbc34617eecea1610af9b18f4030b1023fac
Ahmed Alser <ahmedalser121@gmail.com>
2022-09-20 00:33:30 +0300

    Creat solution of Assignment 4 for submisssion

---
+++ Assignments/Assignment 4.py
@@ -0,0 +1,104 @@
+# ASSIGNMENT 4
+# To comment or uncomment lines of code, select them and press (Ctrl+/)
+"""
+Question 1: Length of List
+"""
+
+# def length(_list):
+#     count = 0
+#     for i in _list:
+#         count += 1
+#     print(count)
+#
+#
+# my_list = ["Ahmed", "Mohammed", 54, "GSG", 24]
+# length(my_list)
+
+"""
+Question 2: Check if there Duplicate values
+"""
+
+# def check():
+#     x = input("Enter first value: ")
+#     y = input("Enter second value: ")
+#     z = input("Enter third value: ")
+#     if x == y or x == z or y == z:
+#         print("DUPLICATES")
+#     else:
+#         print("ALL UNIQUE")
+
+
+# check()
+
+"""
+Question 3: Determine the smallest value
+"""
+
+# def smallest(x, y, z):
+#     if x < y and x < z:
+#         small = x
+#     elif y < x and y < z:
+#         small = y
+#     else:
+#         small = z
+#     print("The smallest value is ", small)
+
+
+#
+#
+# smallest(54, -98, 0)
+
+"""
+Question 4:
+Count the positive and negative values and,
+computes the total and average of it
+"""
+
+# def count():
+#     i = int(input("Enter an integer, enter 0 to stop: "))
+#     p_count = 0
+#     n_count = 0
+#     total = 0
+#     while i != 0:
+#         if i > 0:
+#             p_count += 1
+#         else:
+#             n_count += 1
+#         total += i
+#         i = int(input("Enter the next integer: "))
+#         tot_count = p_count + n_count
+#         average = total / tot_count
+#     print("The number of positive values is", p_count)
+#     print("The number of negative values is", n_count)
+#     print("The total is", total)
+#     print("The average is", float(average))
+# count()
+
+""""""
+
+
+# def count():
+#     i = int(input("Enter an integer, enter 0 to stop: "))
+#     p_count = 0
+#     n_count = 0
+#     total = 0
+#     if i != 0:
+#         while i != 0:
+#             if i > 0:
+#                 p_count += 1
+#             elif i < 0:
+#                 n_count += 1
+#             total += i
+#             i = int(input("Enter the next integer: "))
+#             tot_count = p_count + n_count
+#             average = total / tot_count
+#
+#         print("The number of positive values is", p_count)
+#         print("The number of negative values is", n_count)
+#         print("The total is", total)
+#         print("The average is", float(average))
+#     else:
+#         print("You didn't enter any number.")
+
+
+""""""
