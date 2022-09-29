f5c66f0b9727d11dbb0ba20001d9f030338380a4
Ahmed Alser <ahmedalser121@gmail.com>
2022-09-29 12:16:27 +0300

    Improve the solution

--- Assignments/Assignment 5.py
+++ Assignments/Assignment 5.py
@@ -1,38 +1,38 @@
 class Calculator:
-    def __init__(self, num_1, num_2):
-        self.a = num_1
-        self.b = num_2
+    def __init__(self, first_num, second_num):
+        self.first_num = first_num
+        self.second_num = second_num
 
     def add(self):
-        return self.a + self.b
+        return self.first_num + self.second_num
 
     def subtract(self):
-        return self.a - self.b
+        return self.first_num - self.second_num
 
     def multiply(self):
-        return self.a * self.b
+        return self.first_num * self.second_num
 
     def divide(self):
-        return self.a / self.b
+        return self.first_num / self.second_num
 
     def choice(self, operator):
         if operator == 1:
-            return f" {self.a} + {self.b} = {Calculator.add(self)}"
+            return f" {first_num} + {second_num} = {self.add()}"
         elif operator == 2:
-            return f" {self.a} - {self.b} = {Calculator.subtract(self)}"
+            return f" {first_num} - {second_num} = {self.subtract()}"
         elif operator == 3:
-            return f" {self.a} * {self.b} = {Calculator.multiply(self)}"
+            return f" {first_num} * {second_num} = {self.multiply()}"
         elif operator == 4:
-            return f" {self.a} / {self.b} = {Calculator.divide(self)}"
+            return f" {first_num} / {second_num} = {self.divide()}"
         else:
-            print("Invalid Choice!")
+            return "Invalid Choice!"
 
 
 while True:
-    num_one = int(input("Enter first number: "))
-    num_two = int(input("Enter second number: "))
+    first_num = int(input("Enter first number: "))
+    second_num = int(input("Enter second number: "))
     operator = int(input("Choose operation:\n1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n>> "))
-    calc = Calculator(num_one, num_two)
+    calc = Calculator(first_num, second_num)
     result = calc.choice(operator)
     print(result)
     another_operation = input("Do you want another operation? ")
